469f1ae4c8825a94c73dc9b6e44ce9f4a26fea13
jayliam <51280073+jayliam@users.noreply.github.com>
2021-07-09 19:11:15 +0100

    added coments

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+
+//Vitesse Du Joueur
 var playerVelocity = 150;
+
 var score = 0;
 var lives = 3;
+
+//text Pour Afficher Le Score
 var scoreText;
+
+//text Pour Afficher Les Vies Restantes
 var livesText;
-var TaxiX = 1100;
+
 var VelocityCar1 = 400;
-var initialTime = 500;
+
+//Temp Initial
+var initialTime = 90;
+
 var config = {        
     type: Phaser.AUTO,
     width: 1280,
@@ -37,7 +47,7 @@ function preload(){
     this.load.image("tiles", "nicetiles.png");
     this.load.tilemapTiledJSON("map", "nicemap.json");
 
-    //Vehicles
+    //Vehicules
     this.load.image("Audi", "Audi.png");
     this.load.image("Audi2", "Audi2.png");
     this.load.image("MiniTruck", "Mini_truck.png");
@@ -54,12 +64,17 @@ function preload(){
 }
 
 function collectRedStar (player, star){
+
+    //Ajouter 10 Au Score si le joueur prend une star rouge
     star.disableBody(true, true);
     score = score + 10
     scoreText.setText('Score: ' + score);
     
 }
 function collectStar (player, star){
+
+    //Ajouter 1 Au Score si le joueur prend une star jaune
+
     star.disableBody(true, true);
     score = score + 1
     scoreText.setText('Score: ' + score);
@@ -67,11 +82,11 @@ function collectStar (player, star){
 }
 
 function PlayerVehicleCollision(player, vehicle) {
+    
+    
     if (lives == 1) {
-
         if(alert('You Lose!')){}
         else  window.location.reload();   
-
     }else{
         lives = lives - 1;
         livesText.setText('lives: ' + lives);
@@ -82,13 +97,16 @@ function PlayerVehicleCollision(player, vehicle) {
     
 }
 function DrawVehicles(th){
+
+    //ajout des vehicules
     Audi= th.physics.add.sprite(1200, 290, 'Audi').setScale(0.5).setVelocityX(-VelocityCar1).refreshBody();
     Police= th.physics.add.sprite(100, 10*64 + 34, 'Police').setScale(0.5).setVelocityX(-VelocityCar1).refreshBody();
     MiniTruck= th.physics.add.sprite(750, 11*64 + 34, 'MiniTruck').setScale(0.5).setVelocityX(VelocityCar1).refreshBody();
     Audi2= th.physics.add.sprite(9*64 +34, 450, 'Audi2').setScale(0.5).setVelocityY(VelocityCar1).refreshBody();
-    Taxi= th.physics.add.sprite(TaxiX, 345, 'Taxi').setScale(0.5).setVelocityX(VelocityCar1).refreshBody();
+    Taxi= th.physics.add.sprite(1100, 345, 'Taxi').setScale(0.5).setVelocityX(VelocityCar1).refreshBody();
     Taxi2= th.physics.add.sprite(10*64 +34, 550, 'Taxi2').setScale(0.5).setVelocityY(-VelocityCar1).refreshBody();
 
+    //Mise a jour des boxes des vehicules pour un bon test de colision 
     Audi.body.setSize(210, 90, 50, 25);
     Audi2.body.setSize(90, 210, 50, 25);
     Taxi.body.setSize(210, 90, 50, 25);
@@ -96,6 +114,7 @@ function DrawVehicles(th){
     MiniTruck.body.setSize(210, 90, 50, 25);
     Police.body.setSize(210, 90, 50, 25);
 
+    //appele la fonction PlayerVehicleCollision si une voiture touche le joueur
     th.physics.add.overlap(player, Taxi, PlayerVehicleCollision, null, this);
     th.physics.add.overlap(player, Audi, PlayerVehicleCollision, null, this);
     th.physics.add.overlap(player, Taxi2, PlayerVehicleCollision, null, this);
@@ -107,52 +126,47 @@ function DrawVehicles(th){
 
 function create(){
 
-    //Creating Map
+    //Creation de la map (format JSON Generer par Le Programme Tiled qui aide a dessiner les maps a partir de tileset)
     map = this.make.tilemap({ key: "map", tileWidth: 64, tileHeight: 64});
     tileset = map.addTilesetImage("nicetiles","tiles");
     layer = map.createLayer("TileLayer1", tileset, 0, 0);
     
-    //Player
-    player= this.physics.add.sprite(1200, 100, 'player');
+    //Creer le joueur et metre a jour son box 
+    player= this.physics.add.sprite(100, 100, 'player');
     player.body.setSize(18, 44, 50, 25);
     
-    //Vehicles
+    //afficher les Vehicules
     DrawVehicles(this);
 
-    //Stars
+    //afficher les Stars
     DrawStars(this);
 
-    //Text
+    //affichage du Text du score, Vies restantes et le temp restant
     scoreText = this.add.text(20, 10, 'Score: 0', { font: "32px Consolas", fill: '#000' });
     livesText = this.add.text(20, 40, 'lives: 3', { font: "32px Consolas", fill: '#000' });
-    
-    //Player Collison
+    TimeText = this.add.text(220, 10, 'Time Left: ' + Time(initialTime),{ font: "32px Consolas", fill: '#000' });
+
+
+    //Collison du joueur avec la map ( tiles des routes (id = 2) )
     this.physics.add.collider(player, layer);
     layer.setCollisionBetween(2, 2);
     player.setBounce(0);
     player.setGravity((0,0));
     player.setCollideWorldBounds(true);
 
-    //Go To Function gameover when player hits finish tile id=5
-    //map.setTileIndexCallback(5, gameover, this);
-
-    //Player Animations
+    //Animations du joueur
     playerAnimations();
 
-
     
-
-    TimeText = this.add.text(220, 10, 'Time Left: ' + Time(initialTime),{ font: "32px Consolas", fill: '#000' });
-
- 
-
-    // Each 1000 ms call onEvent
-    timedEvent = this.time.addEvent({ delay: 1000, callback: onEvent, callbackScope: this, loop: true });
+    //metre a jour le temp chaque 1 seconde
+    timedEvent = this.time.addEvent({ delay: 1000, callback: TimeDown, callbackScope: this, loop: true });
 
      
 } 
 
 function Time(S){
+
+    // Fonction qui formate le temp de seconds en MM:SS
     var minutes = Math.floor(S/60);
     var seconds = S%60;
     seconds = seconds.toString().padStart(2,'0');
@@ -160,20 +174,17 @@ function Time(S){
     return string;
 }
 
-function onEvent (){
-    initialTime -= 1; // One second
-    TimeText.setText('Time Left: ' + Time(initialTime));
+function TimeDown (){
+    initialTime -= 1; // Decrementer le temp de 1 seconde
+    TimeText.setText('Time Left: ' + Time(initialTime)); //mettre a jour le text pour l'afficher
     
 }
 
 function checktimeOver() {
     if (initialTime == 0) {
-        
         if(alert('Time is Over You Lose!')){}
         else  window.location.reload();   
-
-    }
-    
+    } 
 }
 
 function gameover() {
@@ -293,6 +304,8 @@ function playerMovement() {
 }
 
 function MoveCars() {
+
+    //verifier et metre a jour la position des vehicules si il sont hors la map 
     if (Audi.x < 0) {
         Audi.setX(1350);
     }
@@ -315,6 +328,10 @@ function MoveCars() {
 
 
 function checkTrafficCollision(th) {
+
+    //verifier si il y a collision entre les vehicules 
+    //( si il y on a un des vehicules doit s'arreter pour que lautre passe )
+
     if(th.physics.overlap(Taxi, Audi2) ){
         Audi2.setVelocityY(0);
     }else{
@@ -359,17 +376,29 @@ function checkTrafficCollision(th) {
     
 }
 function checkWin() {
+
+    //verifier si le joueur est dans la tile de fin
     if (player.x > 1216 && player.y < 30) {
         gameover();
     }
 }
 
 function update(){
+    //verifier et metre a jour la position des vehicules si il sont hors la map 
     MoveCars();
+
+    //verifier si le joueur est dans la tile de fin
     checkWin();
+
+    //verifier si le temp est termine
     checktimeOver();
+
+    //verifier si il y a collision entre les vehicules 
     checkTrafficCollision(this);
+
     cursors = this.input.keyboard.createCursorKeys();
+    
+    //metre a jour la position de player quand le joueur bouge
     playerMovement();
 
 }
